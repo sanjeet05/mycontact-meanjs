146f6e83d6e87f720237430c47440313c723a5e9
Sanjeet Kumar <sanjeet.k@mobillionlabs.com>
2016-10-27 17:27:48 +0530

    wip

---
+++ modules/contacts/client/config/contacts.client.config.js
@@ -0,0 +1,33 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('contacts')
+    .run(menuConfig);
+
+  // menuConfig.$inject = ['menuService'];
+  menuConfig.$inject = ['Menus'];
+
+  function menuConfig(menuService) {
+    // Set top bar menu items
+    // menuService.addMenuItem('topbar', {
+    //   title: 'Contacts',
+    //   state: 'contacts',
+    //   type: 'dropdown',
+    //   roles: ['*']
+    // });
+
+    // Add the dropdown list item
+    // menuService.addSubMenuItem('topbar', 'contacts', {
+    //   title: 'List Contacts',
+    //   state: 'contacts.list'
+    // });
+
+    // Add the dropdown create item
+    // menuService.addSubMenuItem('topbar', 'contacts', {
+    //   title: 'Create Contact',
+    //   state: 'contacts.create',
+    //   roles: ['user']
+    // });
+  }
+}());
